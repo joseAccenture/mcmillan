8d3e61d25117dd186001420670073e3ad523f58e
Sergio Salazar <s.salazarcardoso@accenture.com>
2018-05-08 12:44:28 +0200

    comentario

---
+++ src/app/scripts/app.footer.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-footer',
+  templateUrl: '../views/app.footer.html',
+  styleUrls: ['../styles/app.footer.css']
+})
+export class AppFooter {
+  title = 'app';
+}
---
+++ src/app/scripts/app.navbar.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: '../views/app.navbar.html',
+  styleUrls: ['../styles/app.navbar.css']
+})
+export class AppNavbar {
+  title = 'app';
+}
