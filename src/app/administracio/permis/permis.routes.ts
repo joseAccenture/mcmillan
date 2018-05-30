c7c371542e0572faddc1e8b8c55a7eb28cd89412
Salazar Cardoso <s.salazarcardoso@accenture.com>
2018-05-30 08:14:55 +0200

    delete old folders

--- src/app/administracio/permis/permis.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Routes, RouterModule, CanActivate } from '@angular/router';
-
-import { PermisLlistarComponent } from './components/llistar/permis.llistar.component';
-
-export const permisRoutes: Routes = [
-
-   {path:'', component: PermisLlistarComponent }
-   
-];
