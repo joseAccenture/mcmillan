df1f66ec744d8220aeecbf042325f320fb94687c
Salazar Cardoso <s.salazarcardoso@accenture.com>
2018-06-26 15:58:55 +0200

    Redirect

--- src/app/console/service/console.service.ts
+++ src/app/console/service/console.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs'
-
 import { Headers, Http, RequestOptions } from '@angular/http';
 
 
@@ -18,63 +17,88 @@ const httpOptions = {
   providedIn: 'root',
 })
 export class ConsoleService {
-  public static backendUrl = "macmillanBackend"
+  backendUrl: any;
+  URLactual: string;
+  // public static backendUrl;
+
+  DoRedirect() {
+    this.URLactual = window.location.pathname.slice(1).toString();
+    if (this.URLactual.includes('macmillanBackend')){
+      this.backendUrl = '/macmillanBackend';
+    }else{
+      this.backendUrl = 'http://localhost:8080'
+    }
+
+  }
+
+  
+  
    
 
-  constructor(private http : HttpClient) {
-      
-  }
+  constructor(private http : HttpClient) {}
   
+
+
   getCLients(id: number) {
-        var CLIENTS = this.http.get('/'+ConsoleService.backendUrl+'/customers/'+id, httpOptions);
+    this.DoRedirect();
+        var CLIENTS = this.http.get('/'+this.backendUrl+'/customers/'+id, httpOptions);
          return CLIENTS;
   }
 
   getCLientDetail(numCliente) {
-  
+    this.DoRedirect();
     const headers = new HttpHeaders();
     headers.set('Accept', 'application/json');
     headers.set('Access-Control-Allow-Origin ', 'http://localhost:4200');
     headers.set('Authorization', 'Basic ' + btoa('U23r:MacMillan!1'));
-    var CLIENTS = this.http.get  ('/'+ConsoleService.backendUrl+'/customers/'+numCliente, {headers});
+    var CLIENTS = this.http.get  (this.backendUrl+'/customers/'+numCliente, {headers});
     return CLIENTS;
   }
   submitLine(line) {
-    var LINE = this.http.post('/'+ConsoleService.backendUrl+'/users', line);
+    this.DoRedirect();
+    var LINE = this.http.post(this.backendUrl+'/users', line);
    return LINE;
  } 
   getOrders(id) {
- var ORDER_DETAIL = this.http.get('/'+ConsoleService.backendUrl+'/orders/'+ id, httpOptions);
+    this.DoRedirect();
+ var ORDER_DETAIL = this.http.get(this.backendUrl+'/orders/'+ id, httpOptions);
      return ORDER_DETAIL;
    }
 
   getOrdersList(fecEnt, fecSal, id) {
-    var ORDER_DETAIL = this.http.get('/'+ConsoleService.backendUrl+'/salesorders/customers/'+id+'?numCliente='+id+'&organizacionVentas=0001&fechaDocumentoDesde='+fecEnt+'&fechaDocumentoHasta='+fecSal);
+    this.DoRedirect();
+    var ORDER_DETAIL = this.http.get(this.backendUrl+'/salesorders/customers/'+id+'?numCliente='+id+'&organizacionVentas=0001&fechaDocumentoDesde='+fecEnt+'&fechaDocumentoHasta='+fecSal);
     return ORDER_DETAIL;
   }
   getDeliveryNotes(numDocumentoComercial){
-    var DELIVERY_NOTES = this.http.get('/'+ConsoleService.backendUrl+'/salesorders/'+numDocumentoComercial+'/deliverynotes');
+    this.DoRedirect();
+    var DELIVERY_NOTES = this.http.get(this.backendUrl+'/salesorders/'+numDocumentoComercial+'/deliverynotes');
     return DELIVERY_NOTES;
   }
   getDeliveryNoteDetail(numDocumentoComercial){
-    var DELIVERY_NOTE_DETAIL = this.http.get('/'+ConsoleService.backendUrl+'/deliverynotes/'+numDocumentoComercial);
+    this.DoRedirect();
+    var DELIVERY_NOTE_DETAIL = this.http.get(this.backendUrl+'/deliverynotes/'+numDocumentoComercial);
     return DELIVERY_NOTE_DETAIL;
 
   }
   getOrdersLines() {
-    var ORDER_LINES = this.http.get(' /'+ConsoleService.backendUrl+'/lineas');
+    this.DoRedirect();
+    var ORDER_LINES = this.http.get(this.backendUrl+'/lineas');
     return ORDER_LINES;
   }
   getUsersbyId(id) {
-    var USERS = this.http.get(' /'+ConsoleService.backendUrl+'/users/'+id);
+    this.DoRedirect();
+    var USERS = this.http.get(this.backendUrl+'/users/'+id);
     return USERS;
   }
   getUsers() {
-    var USERS = this.http.get(' /'+ConsoleService.backendUrl+'/users/');
+    this.DoRedirect();
+    var USERS = this.http.get(this.backendUrl+'/users/');
     return USERS;
   }
   getLogin(userToVal) {
-    var USERS = this.http.post(' /'+ConsoleService.backendUrl+'/login', userToVal);
+    this.DoRedirect();
+    var USERS = this.http.post(this.backendUrl+'/login', userToVal);
     return USERS;
   }
 
